897b14e01a5d03ae875d7d0fd5f8f4efbb99b1b8
Matias <Matias@DESKTOP-NSDQDLU>
2020-03-29 17:17:19 -0300

    1ra Versión de la API deployada a AWS Beanstalk

--- puravida-api/src/main/java/com/melchicorp/puravida/PuravidaApiApplication.java
+++ puravida-api/src/main/java/com/melchicorp/puravida/PuravidaApiApplication.java
@@ -2,12 +2,19 @@ package com.melchicorp.puravida;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PuravidaApiApplication {
+public class PuravidaApiApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PuravidaApiApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PuravidaApiApplication.class);
+    }
 
 }
